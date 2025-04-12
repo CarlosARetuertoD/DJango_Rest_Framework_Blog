3b4dcf43324744b586665c4d8d80cad436317012
CarlosARetuertoD <cretuertodelgado@gmail.com>
2025-04-12 18:45:39 -0500

    Blog Changed

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,9 @@
 import os
-import environ
+import environ # type: ignore
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths insidethe project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
